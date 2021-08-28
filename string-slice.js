c167933140a6e3505b2393b9e47a77a970948c08
monjur15 <monjur.git@gmail.com>
2021-08-28 22:36:34 +0600

    comment

---
+++ string-slice.js
@@ -0,0 +1,13 @@
+const anthem = 'amar sonar bangla';
+const word = anthem.split(' ');
+const withOutA = anthem.split('a');
+// console.log(withOutA);
+
+
+// slice
+const smallSlice = anthem.slice(2, 14);
+// console.log(smallSlice);
+
+//subString
+const anotherPart = anthem.substr(5, 5);
+console.log(anotherPart);
