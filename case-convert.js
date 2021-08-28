ee058e23b856120e23bccbf295404aa82ee5239e
monjur15 <monjur.git@gmail.com>
2021-08-28 13:51:26 +0600

    JS String array usefull method

---
+++ case-convert.js
@@ -0,0 +1,12 @@
+const anthem = 'amar sonar bangla';
+const search = 'bangla';
+
+const userInput = 'blackPinK';
+const userName = 'blackPink';
+// console.log(userInput.toLowerCase());
+// console.log(userName.toLowerCase());
+if (userInput.toLowerCase() == userName.toLowerCase()) {
+    console.log('Bingo! User exist');
+} else {
+    console.log('User not find');
+}
